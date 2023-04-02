fb00cf82a9ff3050c40752e8e28132ce6d9af4d5
AllanGd <allan_gd@hotmail.com>
2023-04-02 00:48:04 -0300

    Adjust

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@ import Card from './components/Card.vue';
 </script>
 
 <template>
-  <div class="bg-gray-300 h-screen p-2">
-    <Card />
+  <div class="bg-gray-200 h-screen p-2">
+    <Card class=""/>
   </div>
 </template>
 
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="w-72 p-4 bg-white rounded-md">
+    <div class="w-72 p-4 bg-white rounded-md shadow-xl">
         <div class=" grid grid-cols-2 gap-6">
-            <img src="https://i.pinimg.com/originals/b1/26/fb/b126fb42bf843d90cddb40911fd7e2d6.jpg" alt="foto perfil"
+            <img src="https://cdnb.artstation.com/p/assets/images/images/050/137/691/large/vitaly-perevoshikov-2.jpg?1654134898" alt="foto perfil"
                 class="h-24 w-24 object-cover rounded-full ring-4 ring-offset-2 ring-slate-400">
             <div class="flex flex-col gap-1 justify-center">
-                <div class="text-gray-600 text-base font-bold">
-                    Allan Gedeão
+                <div class="text-gray-600 text-base font-bold capitalize">
+                    Lion El Jonson
                 </div>
-                <div class="text-gray-500 text-sm">Paladino</div>
+                <div class="text-gray-500 text-sm capitalize">Paladino</div>
                 <div class="text-gray-500 text-sm">Lvl 3 / PE 5</div>
             </div>
         </div>
@@ -22,16 +22,16 @@
         <div class="grid grid-cols-2 place-content-between mt-6 gap-2">
             <div>Força
             </div>
-            <div class="text-gray-500 text-end font-bold">3</div>
+            <div class="text-gray-500 text-end font-bold">5</div>
             <div>Habilidade
             </div>
-            <div class="text-gray-500 text-end font-bold">3</div>
+            <div class="text-gray-500 text-end font-bold">2</div>
             <div>Resistência
             </div>
-            <div class="text-gray-500 text-end font-bold">3</div>
+            <div class="text-gray-500 text-end font-bold">2</div>
             <div>Armadura
             </div>
-            <div class="text-gray-500 text-end font-bold">3</div>
+            <div class="text-gray-500 text-end font-bold">1</div>
             <div>Poder de Fogo
             </div>
             <div class="text-gray-500 text-end font-bold">3</div>
