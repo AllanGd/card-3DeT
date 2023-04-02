d74a6c6964eddc3e50c0135fa0e2f936fc926b5c
AllanGd <allan_gd@hotmail.com>
2023-04-02 00:38:08 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Card from './components/Card.vue';
+</script>
+
+<template>
+  <div class="bg-gray-300 h-screen p-2">
+    <Card />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Card.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="w-72 p-4 bg-white rounded-md">
+        <div class=" grid grid-cols-2 gap-6">
+            <img src="https://i.pinimg.com/originals/b1/26/fb/b126fb42bf843d90cddb40911fd7e2d6.jpg" alt="foto perfil"
+                class="h-24 w-24 object-cover rounded-full ring-4 ring-offset-2 ring-slate-400">
+            <div class="flex flex-col gap-1 justify-center">
+                <div class="text-gray-600 text-base font-bold">
+                    Allan Gedeão
+                </div>
+                <div class="text-gray-500 text-sm">Paladino</div>
+                <div class="text-gray-500 text-sm">Lvl 3 / PE 5</div>
+            </div>
+        </div>
+        <div class="mt-4 flex justify-between font-bold">
+            <div>PV
+                <span class="ml-2 text-gray-500">10/10</span>
+            </div>
+            <div>PM
+                <span class="ml-2 text-gray-500">10/10</span>
+            </div>
+        </div>
+        <div class="grid grid-cols-2 place-content-between mt-6 gap-2">
+            <div>Força
+            </div>
+            <div class="text-gray-500 text-end font-bold">3</div>
+            <div>Habilidade
+            </div>
+            <div class="text-gray-500 text-end font-bold">3</div>
+            <div>Resistência
+            </div>
+            <div class="text-gray-500 text-end font-bold">3</div>
+            <div>Armadura
+            </div>
+            <div class="text-gray-500 text-end font-bold">3</div>
+            <div>Poder de Fogo
+            </div>
+            <div class="text-gray-500 text-end font-bold">3</div>
+        </div>
+    </div>
+</template>
